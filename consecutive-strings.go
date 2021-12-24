3a4d3104853b14d99cb8b630202dd1fe4a48b9cb
Piotr <piotr.ostrowski@aol.com>
2021-12-24 17:21:49 +0100

    start on kata

---
+++ consecutive-strings.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func LongestConsecutiveString(strArr []string, k int) string {
+  return strArr[0]
+}
+
+func TestLongestConsecutiveString(t *testing.T) {
+  if {
+  t.Fail()
+  }
+}
+
