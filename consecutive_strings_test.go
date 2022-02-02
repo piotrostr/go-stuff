2c172089f1fc321cd422d3f5b3cf3b8b2e1e9ef0
Piotr <piotr.ostrowski@aol.com>
2022-02-02 02:31:42 +0100

    asdf

--- consecutive_strings_test.go
+++ consecutive_strings_test.go
@@ -2,19 +2,42 @@ package main
 
 import (
   "testing"
-  // "github.com/stretchr/testify/assert"
 )
 
-func LongestConsecutiveString(strArr []string, k int) string {
-  return strArr[0]
+func LongestString(strArr []string, k int) string {
+  var longestSoFar string = strArr[0]
+  for i := 1; i < len(strArr); i++ {
+    if len(strArr[i]) > len(longestSoFar) {
+      longestSoFar = strArr[i]
+    }
+  }
+  return longestSoFar
 }
 
-func TestLongestConsecutiveString(t *testing.T) {
-  strArr := []string{"asdf"}
-  out := LongestConsecutiveString(strArr, 1)
-  if (out != "asdf") {
+func TestLongestString(t *testing.T) {
+  strArr := []string{"asdf", "a", "as", "asd"}
+  out := LongestString(strArr, 1)
+  if out != "asdf" {
     t.Fail()
   }
 }
 
-// func main() {}
+func RemoveSpaces(str string) string {
+  var newStr string = ""
+  for i := 0; i < len(str); i++ {
+    if string(str[i]) != string(" ") {
+      newStr += string(str[i])
+    }
+  }
+  return newStr
+}
+
+func TestRemoveSpaces(t *testing.T) {
+  str := "8 j 8   mBliB8g  imjB8B8  jl  B"
+  noSpacesStr := RemoveSpaces(str)
+  if noSpacesStr != "8j8mBliB8gimjB8B8jlB" {
+    t.Fail()
+  }
+}
+
+func main() {}
--- hello-world.go
+++ hello-world.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func hello() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
 }
-
