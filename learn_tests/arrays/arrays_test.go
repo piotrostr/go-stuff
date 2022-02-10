ad7059e1bec027edd0e1d691c87cc8e253b6c462
Piotr <piotr.ostrowski@aol.com>
2022-02-10 14:44:29 +0100

    make sum pass for arrs of any size

--- learn_tests/arrays/arrays_test.go
+++ learn_tests/arrays/arrays_test.go
@@ -10,13 +10,18 @@ import (
 var _ = Describe("Arrays", func() {
 	Describe("Sum", func() {
 		It("Should sum right for nums equal 15", func() {
-			numbers := [5]int{1, 2, 3, 4, 5}
+			numbers := []int{1, 2, 3, 4, 5}
 			Expect(arrays.Sum(numbers)).To(Equal(15))
 		})
 
 		It("Should sum right for nums equal any sum", func() {
-			numbers := [5]int{2, 2, 3, 4, 5}
+			numbers := []int{2, 2, 3, 4, 5}
 			Expect(arrays.Sum(numbers)).To(Equal(16))
+		})
+
+		It("Should sum right for any array size", func() {
+			numbers := []int{1, 1, 1, 1, 1, 1, 1}
+			Expect(arrays.Sum(numbers)).To(Equal(7))
 		})
 	})
 })
