f0946b76701a68008553c662e2b46b187ac86558
Piotr <piotr.ostrowski@aol.com>
2022-02-10 20:54:05 +0100

    pass dicts cases

--- learn_tests/dictionary/dictionary.go
+++ learn_tests/dictionary/dictionary.go
@@ -1,1 +1,17 @@
 package dictionary
+
+import (
+	"errors"
+)
+
+type Dict map[string]string
+
+var NoSuchWordError = errors.New("no such word")
+
+func Search(dict map[string]string, word string) (string, error) {
+	value, success := dict[word]
+	if !success {
+		return "", NoSuchWordError
+	}
+	return value, nil
+}
--- learn_tests/dictionary/dictionary_test.go
+++ learn_tests/dictionary/dictionary_test.go
@@ -4,11 +4,19 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"learn_tests/dictionary"
+	. "learn_tests/dictionary"
 )
 
 var _ = Describe("Dictionary", func() {
-        It("searches", func() {
-                dict := map[string]string{"key": "value"}
-        }
+	It("returns right word", func() {
+		dict := Dict{"key": "value"}
+		Expect(Search(dict, "key")).To(Equal("value"))
+	})
+
+	It("returns error if word is not there", func() {
+		dict := Dict{"asf": "value"}
+		value, err := Search(dict, "key")
+		Expect(value).To(Equal(""))
+		Expect(err).To(MatchError("no such word"))
+	})
 })
