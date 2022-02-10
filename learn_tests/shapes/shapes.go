ccbc9053a53872fdc50e472e0ed1a9f4088adbef
Piotr <piotr.ostrowski@aol.com>
2022-02-10 15:46:38 +0100

    use methods to implement circle shape methods

--- learn_tests/shapes/shapes.go
+++ learn_tests/shapes/shapes.go
@@ -1,14 +1,30 @@
 package shapes
 
+import (
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Height + rectangle.Width)
+type Circle struct {
+	Radius float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- learn_tests/shapes/shapes_test.go
+++ learn_tests/shapes/shapes_test.go
@@ -5,23 +5,31 @@ import (
 	. "github.com/onsi/gomega"
 
 	"learn_tests/shapes"
+	"math"
 )
 
 var _ = Describe("Shapes", func() {
 	Describe("Perimeter", func() {
 		It("should calculate perimeter right", func() {
 			rectangle := shapes.Rectangle{10.0, 10.0}
-			got := shapes.Perimeter(rectangle)
+			got := rectangle.Perimeter()
 			want := 40.0
 			Expect(got).To(Equal(want))
 		})
 	})
 
 	Describe("Area", func() {
-		It("should calculate area right", func() {
+		It("should calculate rectangle area right", func() {
 			rectangle := shapes.Rectangle{5.0, 10.0}
-			got := shapes.Area(rectangle)
+			got := rectangle.Area()
 			want := 50.0
+			Expect(got).To(Equal(want))
+		})
+
+		It("should calculate circle area right", func() {
+			circle := shapes.Circle{10.0}
+			got := circle.Area()
+			want := 100.0 * math.Pi
 			Expect(got).To(Equal(want))
 		})
 	})
