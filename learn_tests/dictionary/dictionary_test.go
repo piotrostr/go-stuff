435e31f411d22df1937a6a19e40538d19cc04837
Piotr <piotr.ostrowski@aol.com>
2022-02-10 20:35:35 +0100

    add dict case

---
+++ learn_tests/dictionary/dictionary_test.go
@@ -0,0 +1,14 @@
+package dictionary_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"learn_tests/dictionary"
+)
+
+var _ = Describe("Dictionary", func() {
+        It("searches", func() {
+                dict := map[string]string{"key": "value"}
+        }
+})
