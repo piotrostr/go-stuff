d4c252b076cc36dc9edcaaba85b16ef98609d5b3
Piotr <piotr.ostrowski@aol.com>
2022-02-10 23:41:12 +0100

    use mock servers

--- learn_tests/website_racer/website_racer_test.go
+++ learn_tests/website_racer/website_racer_test.go
@@ -2,6 +2,9 @@ package website_racer_test
 
 import (
 	"learn_tests/website_racer"
+	"net/http"
+	"net/http/httptest"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -9,11 +12,23 @@ import (
 
 var _ = Describe("WebsiteRacer", func() {
 	It("races", func() {
-		slowURL := "http://www.facebook.com"
-		fastURL := "http://www.quii.dev"
+		slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(20 * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
 		got := website_racer.Racer(slowURL, fastURL)
 
 		Expect(got).To(Equal(fastURL))
+
+		slowServer.Close()
+		fastServer.Close()
 	})
 })
