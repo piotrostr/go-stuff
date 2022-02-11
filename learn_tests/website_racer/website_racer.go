ce92e63f2dc6eca26c72a707c0a615e68c7c4c96
Piotr <piotr.ostrowski@aol.com>
2022-02-11 11:54:08 +0100

    use channels and refactor

--- learn_tests/website_racer/website_racer.go
+++ learn_tests/website_racer/website_racer.go
@@ -2,20 +2,25 @@ package website_racer
 
 import (
 	"net/http"
-	"time"
 )
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func Racer(url1 string, url2 string) (winner string) {
+	select {
+
+	case <-ping(url1):
+		return url1
+
+	case <-ping(url2):
+		return url2
+
+	}
 }
 
-func Racer(url1 string, url2 string) (winner string) {
-	duration1 := measureResponseTime(url1)
-	duration2 := measureResponseTime(url2)
-	if duration1 < duration2 {
-		return url1
-	}
-	return url2
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
--- learn_tests/website_racer/website_racer_test.go
+++ learn_tests/website_racer/website_racer_test.go
@@ -10,16 +10,13 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("WebsiteRacer", func() {
+var _ = Describe("Racer", func() {
 	It("races", func() {
-		slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(20 * time.Millisecond)
-			w.WriteHeader(http.StatusOK)
-		}))
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-		fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-		}))
+		defer slowServer.Close()
+		defer fastServer.Close()
 
 		slowURL := slowServer.URL
 		fastURL := fastServer.URL
@@ -27,8 +24,14 @@ var _ = Describe("WebsiteRacer", func() {
 		got := website_racer.Racer(slowURL, fastURL)
 
 		Expect(got).To(Equal(fastURL))
-
-		slowServer.Close()
-		fastServer.Close()
 	})
 })
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				time.Sleep(delay)
+				w.WriteHeader(http.StatusOK)
+			}))
+}
