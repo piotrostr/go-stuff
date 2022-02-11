ff2c4ee203619963516ac6ba6c64b4af4c53a920
Piotr <piotr.ostrowski@aol.com>
2022-02-11 12:11:05 +0100

    channels example

---
+++ channels/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func sendDataToChannel(ch chan string, s string) {
+	ch <- s
+	close(ch)
+}
+
+func main() {
+	ch := make(chan string)
+
+	go sendDataToChannel(ch, "hello")
+
+	value, ok := <-ch
+
+	if !ok {
+		fmt.Println("channel closed")
+	}
+
+	fmt.Println(value)
+}
