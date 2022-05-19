e4b7ff69fed1202f0995e1dce785a331a91df34e
piotrostr <piotr.ostrowski@aol.com>
2022-05-19 14:36:56 +0200

    minimal server

--- api/main.go
+++ api/main.go
@@ -7,18 +7,17 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
+	w.Write([]byte("hello"))
 }
 
-func handleRequests() {
-	port := 6969
-	host := "localhost:"
+func runServer() {
+	port := 8000
+	host := "0.0.0.0:"
 	http.HandleFunc("/", homePage)
 	fmt.Printf("listening on localhost:%d", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s%d", host, port), nil))
 }
 
 func main() {
-	handleRequests()
+	runServer()
 }
